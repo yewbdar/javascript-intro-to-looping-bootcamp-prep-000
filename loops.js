3f5fa748209ddf3399cda8f22db9b25de28041c1
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-07-22 17:24:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -13,12 +13,14 @@ function whileLoop(n){
     console.log('done')
        n--;
   }
-   let i=0;
+   
+}
+
+let i=0;
   function incrementVariable(){
     i =i+1;
     return i
   }
-}
  function doWhileLoop(n){
    do{
      console.log('I run once regardless')
